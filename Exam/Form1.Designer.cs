0f0cb0ec4f18e9188bf70adf4d18cd90634113c6
nadatayebi <nada.tayebi@usmba.ac.ma>
2022-12-04 02:17:44 +0100

    https://github.com/nadatayebi/Exam

--- Exam/Form1.Designer.cs
+++ Exam/Form1.Designer.cs
@@ -34,6 +34,18 @@ namespace Exam
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.rjButton2 = new Exam.RJButton();
+            this.rjButton3 = new Exam.RJButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id_exam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nom_exam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.refresh = new Exam.RJButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -84,10 +96,146 @@ namespace Exam
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(-122, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(219, 453);
+            this.panel1.Size = new System.Drawing.Size(316, 453);
             this.panel1.TabIndex = 3;
+            // 
+            // rjButton2
+            // 
+            this.rjButton2.BackColor = System.Drawing.SystemColors.Info;
+            this.rjButton2.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.rjButton2.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjButton2.BorderRadius = 13;
+            this.rjButton2.BorderSize = 0;
+            this.rjButton2.FlatAppearance.BorderSize = 0;
+            this.rjButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton2.Font = new System.Drawing.Font("Simplified Arabic Fixed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rjButton2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rjButton2.Location = new System.Drawing.Point(482, 136);
+            this.rjButton2.Name = "rjButton2";
+            this.rjButton2.Size = new System.Drawing.Size(188, 49);
+            this.rjButton2.TabIndex = 22;
+            this.rjButton2.Text = "Add questions";
+            this.rjButton2.TextColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rjButton2.UseVisualStyleBackColor = false;
+            this.rjButton2.Click += new System.EventHandler(this.rjButton2_Click);
+            // 
+            // rjButton3
+            // 
+            this.rjButton3.BackColor = System.Drawing.Color.DarkOrange;
+            this.rjButton3.BackgroundColor = System.Drawing.Color.DarkOrange;
+            this.rjButton3.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjButton3.BorderRadius = 13;
+            this.rjButton3.BorderSize = 0;
+            this.rjButton3.FlatAppearance.BorderSize = 0;
+            this.rjButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton3.Font = new System.Drawing.Font("Simplified Arabic Fixed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rjButton3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rjButton3.Location = new System.Drawing.Point(237, 136);
+            this.rjButton3.Name = "rjButton3";
+            this.rjButton3.Size = new System.Drawing.Size(145, 49);
+            this.rjButton3.TabIndex = 21;
+            this.rjButton3.Text = "Confirm";
+            this.rjButton3.TextColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rjButton3.UseVisualStyleBackColor = false;
+            this.rjButton3.Click += new System.EventHandler(this.rjButton3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox2.Location = new System.Drawing.Point(482, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 31);
+            this.textBox2.TabIndex = 20;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(273, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 31);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.label3.Font = new System.Drawing.Font("Simplified Arabic Fixed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(482, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 24);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "exam name    ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.label2.Font = new System.Drawing.Font("Simplified Arabic Fixed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(237, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 24);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "exam number";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_exam,
+            this.nom_exam});
+            this.dataGridView1.GridColor = System.Drawing.Color.Maroon;
+            this.dataGridView1.Location = new System.Drawing.Point(436, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(365, 225);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // id_exam
+            // 
+            this.id_exam.HeaderText = "id_exam";
+            this.id_exam.MinimumWidth = 8;
+            this.id_exam.Name = "id_exam";
+            this.id_exam.Width = 150;
+            // 
+            // nom_exam
+            // 
+            this.nom_exam.HeaderText = "nom_exam";
+            this.nom_exam.MinimumWidth = 8;
+            this.nom_exam.Name = "nom_exam";
+            this.nom_exam.Width = 150;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "Column1";
+            this.id.MinimumWidth = 8;
+            this.id.Name = "id";
+            this.id.Width = 150;
+            // 
+            // refresh
+            // 
+            this.refresh.BackColor = System.Drawing.Color.DarkOrange;
+            this.refresh.BackgroundColor = System.Drawing.Color.DarkOrange;
+            this.refresh.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.refresh.BorderRadius = 13;
+            this.refresh.BorderSize = 0;
+            this.refresh.FlatAppearance.BorderSize = 0;
+            this.refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.refresh.Font = new System.Drawing.Font("Simplified Arabic Fixed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.refresh.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.refresh.Location = new System.Drawing.Point(252, 389);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(145, 49);
+            this.refresh.TabIndex = 24;
+            this.refresh.Text = "refresh";
+            this.refresh.TextColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.refresh.UseVisualStyleBackColor = false;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
             // 
             // fstPage
             // 
@@ -96,6 +244,14 @@ namespace Exam
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.rjButton2);
+            this.Controls.Add(this.rjButton3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -104,7 +260,10 @@ namespace Exam
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "fstPage";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.fstPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -114,6 +273,17 @@ namespace Exam
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Panel panel1;
+        private RJButton rjButton2;
+        private RJButton rjButton3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_exam;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nom_exam;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private RJButton refresh;
     }
 }
 
--- Exam/Form1.cs
+++ Exam/Form1.cs
@@ -7,15 +7,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Exam.classes;
+using System.Data.SqlClient;
+// Data Source = DESKTOP - RVGEFLO; Initial Catalog = examManager; Integrated Security = True
+
 
 namespace Exam
 {
     public partial class fstPage : Form
     {
+        //attributes
+
+        public static string idexam = "";
+        public static string nomexam = "";
+        DataAcs ac = new DataAcs();
+        string query;
+        
+       
+
+
         public fstPage()
         {
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void fstPage_Load(object sender, EventArgs e)
+        {
+            query = "select * from Exam";
+            SqlDataReader rd;
+            rd = ac.getComboGrid(query);
+            if (rd.HasRows)
+            {
+
+                this.dataGridView1.Rows.Clear();
+                while (rd.Read())
+                {
+                    this.dataGridView1.Rows.Add(rd["id_exam"], rd["nom_exam"]);
+
+                }
+            }
+        }
+        
+            private void rjButton3_Click(object sender, EventArgs e)
+        {
+            query = "insert into Exam(id_exam, nom_exam) values('" + textBox1.Text + "', '" + textBox2.Text + "')";
+            ac.setData(query, "");
+
+        }
+      
+      
+
+        private void rjButton2_Click(object sender, EventArgs e)
+        {
+            idexam = textBox1.Text;
+            Form31 fr = new Form31();
+            fr.ShowDialog();
+          
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            query = "select * from Exam";
+            SqlDataReader rd;
+            rd = ac.getComboGrid(query);
+            if (rd.HasRows)
+            {
+
+                this.dataGridView1.Rows.Clear();
+                while (rd.Read())
+                {
+                    this.dataGridView1.Rows.Add(rd["id_exam"], rd["nom_exam"]);
+
+                }
+            }
+            else
+                MessageBox.Show("empty");
+
+
+
+
+        
+    }
     }
 }
